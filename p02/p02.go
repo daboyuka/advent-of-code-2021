a310dfb82e839256cc64f33bf6e1eb1781652a5b
Drew Boyuka <drew@pendo.io>
2021-12-02 00:04:39 -0500

    Problem 2 solution

--- p02/p02.go
+++ p02/p02.go
@@ -3,22 +3,45 @@ package p02
 import (
 	"fmt"
 	"io"
-    "strconv"
-    "strings"
 
 	. "aoc2021/helpers"
 )
 
 func A(in io.Reader) {
-    lines := ReadLines(in)
-    _ = lines
+	lines := ReadLines(in)
+	x, d := 0, 0
+	for _, l := range lines {
+		dir, amt := Split2(l, " ")
+		amtV := Atoi(amt)
+		switch dir {
+		case "forward":
+			x += amtV
+		case "down":
+			d += amtV
+		case "up":
+			d -= amtV
+		}
+	}
+	fmt.Println(x, d)
+	fmt.Println(x * d)
 }
 
 func B(in io.Reader) {
-    lines := ReadLines(in)
-    _ = lines
+	lines := ReadLines(in)
+	x, d, a := 0, 0, 0
+	for _, l := range lines {
+		dir, amt := Split2(l, " ")
+		amtV := Atoi(amt)
+		switch dir {
+		case "forward":
+			x += amtV
+			d += amtV * a
+		case "down":
+			a += amtV
+		case "up":
+			a -= amtV
+		}
+	}
+	fmt.Println(x, d)
+	fmt.Println(x * d)
 }
-
-//
-
-var _, _, _ = fmt.Sprintf, strings.Split, strconv.ParseFloat // use something from imports so we can always have them in our template
