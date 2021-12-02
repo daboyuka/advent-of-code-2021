634729ab0dd034d584bf9b2ee81518803ac3d72c
Drew Boyuka <drew@pendo.io>
2021-12-01 23:10:47 -0500

    Upgrade get.sh to label files by part

--- get.sh
+++ get.sh
@@ -9,8 +9,9 @@ done
 
 curl -b ~/.aoc_cookies https://adventofcode.com/2021/day/$DAY -o - | \
   awk '
-    BEGIN{num=0;}
+    BEGIN{num=0;part="a";}
+    /<h2 id="part2">/{part="b";}
     /<pre><code>/{gsub("<pre><code>",""); on=1;}
     /<\/code><\/pre>/{on=0;num++;}
-    on{file = "inputs/'$DAY'sample" num ".input"; print > file;}
+    on{file = "inputs/'$DAY'" part ".sample" num ".input"; print > file;}
   '
