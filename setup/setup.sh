2f1e0bfc5fade40937b9653f3a908764f0d9c9de
Drew Boyuka <drew@pendo.io>
2021-12-04 14:08:26 -0500

    Added documentation to README.md

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,4 +6,9 @@ for ((i=1;i<=25;i++)); do
   j=$(2d $i);
   mkdir -p p$j
   sed "s/XXX/$j/" setup/template.go.txt > p$j/p$j.go
+  for p in a b; do
+    jqf=p$j/p$j$p.jq
+    cp setup/template.jq.txt $jqf
+    chmod +x $jqf
+  done
 done
