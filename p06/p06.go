1046008282578b34518a0c1279cefea55a15eaa7
Drew Boyuka <drew@pendo.io>
2021-12-06 00:06:48 -0500

    Problem 6 solution

--- p06/p06.go
+++ p06/p06.go
@@ -3,22 +3,46 @@ package p06
 import (
 	"fmt"
 	"io"
-    "strconv"
-    "strings"
+	"strings"
 
 	. "aoc2021/helpers"
 )
 
+func evolve(fishAtAge []int) []int {
+	newFish := fishAtAge[0]
+
+	newFishAtAge := fishAtAge[1:] // all ages shift down
+	newFishAtAge[6] += newFish
+	newFishAtAge = append(newFishAtAge, newFish)
+	return newFishAtAge
+}
+
 func A(in io.Reader) {
-    lines := ReadLines(in)
-    _ = lines
+	fishAtAge := make([]int, 9)
+
+	for _, age := range Ints(strings.Split(ReadLines(in)[0], ",")) {
+		fishAtAge[age]++
+	}
+
+	for i := 0; i < 80; i++ {
+		fishAtAge = evolve(fishAtAge)
+	}
+
+	fmt.Println(fishAtAge)
+	fmt.Println(Sum(fishAtAge...))
 }
 
 func B(in io.Reader) {
-    lines := ReadLines(in)
-    _ = lines
+	fishAtAge := make([]int, 9)
+
+	for _, age := range Ints(strings.Split(ReadLines(in)[0], ",")) {
+		fishAtAge[age]++
+	}
+
+	for i := 0; i < 256; i++ {
+		fishAtAge = evolve(fishAtAge)
+	}
+
+	fmt.Println(fishAtAge)
+	fmt.Println(Sum(fishAtAge...))
 }
-
-//
-
-var _, _, _ = fmt.Sprintf, strings.Split, strconv.ParseFloat // use something from imports so we can always have them in our template
