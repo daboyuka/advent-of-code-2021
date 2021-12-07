0d3ca4796a1ff9531d99da26db9691228313052b
Drew Boyuka <drew@pendo.io>
2021-12-07 00:11:24 -0500

    Problem 7 solution

--- p07/p07.go
+++ p07/p07.go
@@ -3,22 +3,57 @@ package p07
 import (
 	"fmt"
 	"io"
-    "strconv"
-    "strings"
+	"strings"
 
 	. "aoc2021/helpers"
 )
 
 func A(in io.Reader) {
-    lines := ReadLines(in)
-    _ = lines
+	poss := Ints(strings.Split(ReadLines(in)[0], ","))
+
+	cnts := map[int]int{}
+	for _, pos := range poss {
+		cnts[pos]++
+	}
+
+	sum := Sum(poss...)
+	total := len(poss)
+	behind := 0
+	minSum := sum
+	minSumAt := 0
+	for i := 1; i <= Max(poss...); i++ {
+		behind += cnts[i-1]
+
+		sum += behind - (total - behind)
+		if sum < minSum {
+			minSum = sum
+			minSumAt = i
+		}
+	}
+	fmt.Println(minSumAt, minSum)
 }
 
 func B(in io.Reader) {
-    lines := ReadLines(in)
-    _ = lines
+	poss := Ints(strings.Split(ReadLines(in)[0], ","))
+
+	posCounts := map[int]int{}
+
+	for _, pos := range poss {
+		posCounts[pos]++
+	}
+
+	minSum := 1<<31 - 1
+	minSumAt := 0
+	for i := 0; i <= Max(poss...); i++ {
+		sum := 0
+		for p, c := range posCounts {
+			d := Abs(p - i)
+			sum += d * (d + 1) / 2 * c
+		}
+		if sum < minSum {
+			minSum = sum
+			minSumAt = i
+		}
+	}
+	fmt.Println(minSumAt, minSum)
 }
-
-//
-
-var _, _, _ = fmt.Sprintf, strings.Split, strconv.ParseFloat // use something from imports so we can always have them in our template
